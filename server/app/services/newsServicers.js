cdb94a10bd3bbd069a6bf1de35e3254ed71a828f
GauMeo123 <mon251096@gmail.com>
2018-11-10 15:39:08 +0700

    no message

--- server/app/services/newsServicers.js
+++ server/app/services/newsServicers.js
@@ -27,62 +27,63 @@ var loadAllData = allFeedbacks.forEach(function (news) {
     });
 });
 
-// exports.getlist = function () {
-//     return new Promise(function (resolve, reject) {
-//         var params = {
-//             TableName: 'Users',
-//
-//         };
-//         return docClient.scan(params).promise().then(result => {
-//             if (result == null) {
-//                 throw {
-//                     message: errors.TEMPLATE_01,
-//                     code: 'TEMPLATE_01'
-//                 };
-//             }
-//             return resolve(result);
-//         })
-//             .catch(error => {
-//                 logger.error(error);
-//                 return reject(error);
-//             });
-//     });
-// };
-// exports.Deletenews =function (id) {
-//     return new Promise(function (resolve, reject) {
-//
-//         var params = {
-//             TableName: 'Users',
-//             Key:{
-//                 "news_id": id
-//             },
-//             ConditionExpression:"info.rating <= :val",
-//             ExpressionAttributeValues: {
-//                 ":val": 8.0
-//             },
-//
-//         };
-//         docClient.query(request_id).then(news_id => {
-//             if (!news_id) {
-//                 console.log(news_id);
-//                 throw {
-//                     message: errors.TEMPLATE_01,
-//                     code: 'TEMPLATE_01'
-//                 };
-//             }
-//
-//             var params = {
-//                 TableName: "Users",
-//                 Item: data
-//             };
-//
-//             return docClient.delete(params).catch(error => {
-//             logger.error(error);
-//             return reject(error);
-//         });
-//     })
-//     });
-// };
+ exports.getlist = function () {
+    return new Promise(function (resolve, reject) {
+        var params = {
+           TableName: 'Users',
+            KeyConditionExpression: "#id:= yyyy",
+            ExpressionAttributeNames:{  "#id": "id"}
+        };
+        return docClient.scan(params).promise().then(result => {
+           if (result == null) {
+               throw {
+                   message: errors.TEMPLATE_01,
+                   code: 'TEMPLATE_01'
+              };
+             }
+           return resolve(result);
+        })
+           .catch(error => {
+               logger.error(error);
+                return reject(error);
+            });
+  });
+ };
+ exports.Deletenews =function (id) {
+    return new Promise(function (resolve, reject) {
+
+        var params = {
+          TableName: 'Users',
+             Key:{
+               "news_id": id
+             },
+             ConditionExpression:"info.rating <= :val",
+             ExpressionAttributeValues: {
+                 ":val": 8.0
+             },
+
+        };
+         docClient.query(request_id).then(news_id => {
+            if (!news_id) {
+                console.log(news_id);
+               throw {
+                    message: errors.TEMPLATE_01,
+                    code: 'TEMPLATE_01'
+                };
+          }
+
+           var params = {
+                TableName: "Users",
+                Item: data
+            };
+
+            return docClient.delete(params).catch(error => {
+            logger.error(error);
+             return reject(error);
+         });
+     })
+     });
+ };
 // exports.Getlastestnews =function (date) {
 //     return new Promise(function (resolve, reject) {
 //
