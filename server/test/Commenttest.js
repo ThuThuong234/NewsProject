fa61e7b59d68606566c142c02289115f973d9936
Dat <datphan.fitiuh@gmail.com>
2018-12-10 11:13:38 +0700

    Commit Code test Comment

--- server/test/Commenttest.js
+++ server/test/Commenttest.js
@@ -1,25 +1,25 @@
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let comment = require('../api/routes/comment');
-let index = require('../api/routes/index');
 let should = chai.should();
-
 chai.use(chaiHttp);
-describe('Comments', () => {
-    beforeEach((done) => {
+describe('Comments', function(){
+    beforeEach(function(done) {
+        done();
+    });
+    afterEach(function (done) {
         done();
     });
     describe('/POST comment', () => {
-        it('it should POST a comment ', function (done) {
+        it('it should POST a comment', function(done){
             chai.request(comment)
                 .post('/')
                 .send({
-                    "email" : "thuong@gmail.com",
-                    "news_id": 1,
-                    "comments_content":"Gau Meo",
-                    "comment_time": "13/12/2018"]
+                    'email': 'datphan.fitiuh@gmail.com',
+                    'comments_content':'Chúng tôi muốn làm việc với các bạn',
+                    'comment_time':'09/12/2018'
                 })
-                .end(function (err, res) {
+                .end(function(err, res){
                     should.equal(err, null);
                     res.should.have.status(200);
                     res.should.be.json;
@@ -29,10 +29,10 @@ describe('Comments', () => {
         }).timeout(5000);
     });
     describe('/GET/:id ', function () {
-        it('it should GET a comment by the given news_id', function (done) {
-            let id = "1";
+        it('it should GET a comment by the given new_id', function (done) {
+            let new_id = "7";
             chai.request(comment)
-                .get('/' + news_id)
+                .get('/' + new_id)
                 .end(function (err, res) {
                     res.should.have.status(200);
                     res.should.be.json;
@@ -41,3 +41,37 @@ describe('Comments', () => {
                 });
         }).timeout(5000);
     });
+    describe('/PUT/:id ', function () {
+        it('should UPDATE a comment given the id', function (done) {
+            let id = '7';
+            chai.request(comment)
+                .put('/' + id )
+                .send({
+                    'news_id': id,
+                    'email': 'thuong10@gmail.com',
+                    'comment_time': '09/12/2018'
+                })
+                .end(function(err, res)
+                {
+                    should.equal(err, null);
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    res.body.should.be.a('object');
+                    done();
+                });
+        }).timeout(5000);
+    });
+    describe('/DELETE/:id ', function () {
+        it('it should DELETE a comment given the id', function (done) {
+            let id = "1544290977610";
+            chai.request(comment)
+                .delete('/' + id)
+                .end(function(err, res){
+                    should.equal(err, null);
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    done();
+                });
+        }).timeout(5000);
+    });
+});
